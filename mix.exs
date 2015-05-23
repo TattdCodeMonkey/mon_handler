08c961883089471920df4a3eeaaf8b6e4aab8bf5
Rodney Norris <rnorris@myemma.com>
2015-05-22 21:29:20 -0500

    bumped patch version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule MonHandler.Mixfile do
 
   def project do
     [app: :mon_handler,
-     version: "1.0.0",
+     version: "1.0.1",
      elixir: "~> 1.0",
      deps: deps,
      description: description,
